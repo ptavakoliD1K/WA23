67b8defd6359296bc5b867716816c986d18f8108
Eike Finster <e.finster@tu-bs.de>
2025-01-24 12:47:57 +0100

    Server test

--- Project WelfenHub/Code/src/main/java/com/welfenhub/services/UserService.java
+++ Project WelfenHub/Code/src/main/java/com/welfenhub/services/UserService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 
