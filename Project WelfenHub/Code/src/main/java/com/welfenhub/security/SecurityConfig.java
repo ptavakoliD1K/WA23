f7f51e5faa3f526cec60f9948ba52ac9aa3c694b
Eike Finster <e.finster@tu-bs.de>
2025-01-26 17:01:01 +0100

    Server Test

--- Project WelfenHub/Code/src/main/java/com/welfenhub/security/SecurityConfig.java
+++ Project WelfenHub/Code/src/main/java/com/welfenhub/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 
 @Configuration
 @EnableWebSecurity
