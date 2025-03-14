33671b51bb5af5af08f76ddeee4561352c877f8b
Eike Finster <eikefinster0205@gmail.com>
2025-03-14 11:43:13 +0100

    Added Group Chat Funktions

---
+++ Project WelfenHub/Code/src/main/java/com/welfenhub/dto/RenameRequestDTO.java
@@ -0,0 +1,13 @@
+package com.welfenhub.dto;
+
+public class RenameRequestDTO {
+    private String newName;
+
+    public String getNewName() {
+        return newName;
+    }
+
+    public void setNewName(String newName) {
+        this.newName = newName;
+    }
+}
