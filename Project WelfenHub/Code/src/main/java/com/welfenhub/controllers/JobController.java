b42d7fbf8eed0e434d4991eb1896063a57b4dd91
dabel <dennis.abel@swisslife.de>
2025-03-14 11:54:00 +0100

    finalized styling job page; added javaDoc

--- Project WelfenHub/Code/src/main/java/com/welfenhub/controllers/JobController.java
+++ Project WelfenHub/Code/src/main/java/com/welfenhub/controllers/JobController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * rest-controller for job page
+ */
+
 @RestController
 @RequestMapping("/job")
 public class JobController {
@@ -14,15 +18,30 @@ public class JobController {
     @Autowired
     JobService jobService;
 
+    /**
+     * post job
+     * @param jobDTO
+     */
+
     @PostMapping("/post")
     public void postJob(@RequestBody JobDTO jobDTO) {
         jobService.saveJob(jobDTO.getTitle(), jobDTO.getContent(), jobDTO.getUrl());
     }
 
+    /**
+     * get job
+     * @return
+     */
+
     @GetMapping("/get")
     public List<JobDTO> getJobs() {
         return jobService.getAllJobs();
     }
+
+    /**
+     * deletes job
+     * @param jobDTO
+     */
 
     @DeleteMapping("/delete")
     public void deleteJob(@RequestBody JobDTO jobDTO) {
