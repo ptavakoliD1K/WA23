28ae38bf98401fb7c48ea4f775e1ea04ea347957
Eike Finster <e.finster@tu-bs.de>
2025-04-23 15:56:50 +0200

    Textlänge Ereignis

--- Project WelfenHub/Code/src/main/java/com/welfenhub/models/Event.java
+++ Project WelfenHub/Code/src/main/java/com/welfenhub/models/Event.java
@@ -13,7 +13,7 @@ public class Event {
     @Column(name = "title")
     private String title;
 
-    @Column(name = "content")
+    @Column(name = "content", columnDefinition = "TEXT")
     private String content;
 
     @Column(name = "date")
